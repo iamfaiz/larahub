62eb294151901575b0f8d81d5b490e1e6ba65863
Rana Faiz Ahmed <iamfaizahmed123@gmail.com>
2014-11-27 14:40:02 +0300

    Forms Folder in views

---
+++ app/views/forms/registeration.blade.php
@@ -0,0 +1,26 @@
+@if (Session::has('messages'))
+<div class="alert alert-danger">
+	<ul>
+		@foreach (Session::get('messages')->all() as $message)
+			<li>{{ $message }}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+{{ Form::open([ 'route' => 'register_path' ]) }}
+<div class="form-group">
+	<label for="username">Username:</label>
+	<input type="text" id="username" name="username" class="form-control" value="" required="required">
+</div>
+<div class="form-group">
+	<label for="email">Email:</label>
+	<input type="email" id="email" name="email" class="form-control" value="" required="required">
+</div>
+<div class="form-group">
+	<label for="password">Password:</label>
+	<input type="password" id="password" name="password" class="form-control" value="" required="required">
+</div>
+<div class="form-group">
+	<button class="btn btn-primary">Sign Up</button>
+</div>
+{{ Form::close() }}
--- app/views/registeration/create.blade.php
+++ app/views/registeration/create.blade.php
@@ -3,32 +3,9 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-6">
-			@if (Session::has('messages'))
-				<div class="alert alert-danger">
-					<ul>
-						@foreach (Session::get('messages')->all() as $message)
-							<li>{{ $message }}</li>
-						@endforeach
-					</ul>
-				</div>
-			@endif
-			{{ Form::open([ 'route' => 'register_path' ]) }}
-				<div class="form-group">
-					<label for="username">Username:</label>
-					<input type="text" id="username" name="username" class="form-control" value="" required="required">
-				</div>
-				<div class="form-group">
-					<label for="email">Email:</label>
-					<input type="email" id="email" name="email" class="form-control" value="" required="required">
-				</div>
-				<div class="form-group">
-					<label for="password">Password:</label>
-					<input type="password" id="password" name="password" class="form-control" value="" required="required">
-				</div>
-				<div class="form-group">
-					<button class="btn btn-primary">Sign Up</button>
-				</div>
-			{{ Form::close() }}
+			<h1>Register</h1>
+			<p class="text-muted">Qucikly fillout these fields to sign yourself in.</p>
+			@include('forms.registeration')
 		</div>
 	</div>
 @stop
